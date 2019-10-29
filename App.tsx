64ff596c861a5aa29b4d0012d0fc99af3d7bd316
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2019-10-29 08:45:45 -0700

    Added react-native-ui-kitten

--- App.tsx
+++ App.tsx
@@ -1,41 +1,13 @@
 import React from 'react';
-import { AppLoading } from 'expo';
-import { Container, Text } from 'native-base';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
+import { mapping, light as lightTheme } from '@eva-design/eva';
+import { ApplicationProvider } from 'react-native-ui-kitten';
+import { IndexComponent } from './src';
 
-interface IState {
-  isReady: boolean;
-}
 
-interface IProps { }
+const App = () => (
+  <ApplicationProvider mapping={mapping} theme={lightTheme}>
+    <IndexComponent />
+  </ApplicationProvider>
+);
 
-export default class App extends React.Component<IProps, IState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isReady: false,
-    };
-  }
-
-  async componentDidMount() {
-    await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font,
-    });
-    this.setState({ isReady: true });
-  }
-
-  render() {
-    if (!this.state.isReady) {
-      return <AppLoading />;
-    }
-
-    return (
-      <Container>
-        <Text>Open up App.js to start working on your app!</Text>
-      </Container>
-    );
-  }
-}
+export default App;
